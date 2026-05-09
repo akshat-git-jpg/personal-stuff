fbb71f490fd4c113d8e160f0a09aa131042c1065
akshat-git-jpg <akshatparty17@gmail.com>
2026-05-09 14:12:15 +0530

    chore: pytest infra + CF link-tracker env placeholders

---
+++ yt-analysis/tests/conftest.py
@@ -0,0 +1,8 @@
+"""Add myproj root to sys.path so `from common.x import y` works in tests."""
+
+import os
+import sys
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
